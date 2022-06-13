b063c593382b52255c390e295a83992501746374
Sávyo Souza <savyosilv@gmail.com>
2022-06-13 15:49:59 -0500

    S6 - Routes

---
+++ resources/views/site/about.blade.php
@@ -0,0 +1,13 @@
+<h3>Coisas sobre nozes (view)</h3>
+
+<ul>
+    <li>
+        <a href="{{ route('site.index') }}">Principal</a>
+    </li>
+    <li>
+        <a href="{{ route('site.sobrenos') }}">Sobre nozes</a>
+    </li>
+    <li>
+        <a href="{{ route('site.contato') }}">Contato</a>
+    </li>
+</ul>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\PrincipalController;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,14 +16,37 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/',[PrincipalController::class, 'principal'])->name('site.index');
+
+Route::get('/about', [PrincipalController::class, 'about'])->name('site.sobrenos');
+
+Route::get('/contact', [PrincipalController::class, 'contact'])->name('site.contato');
+
+Route::get('/login', function (){ return "Login"; })->name('site.login');
+
+Route::prefix('/app')->group(function (){
+    Route::get('/clientes', function (){ return "Clientes"; })->name('app.clientes');
+    Route::get('/fornecedores', function (){ return "fornecedores"; })->name('fornecedores');
+    Route::get('/produtos', function (){ return "produtos"; })->name('produtos');
 });
 
-Route::get('/aboout', function() {
-    return 'Coisas sobre nozes';
-});
+Route::get(
+    '/contact/{nome}/{categoria_id?}',
+    function(
+        String $nome,
+        int $categoria_id = 1 // 1 - informação
+        ) {
+    echo "Chegamos aqui: $nome - $categoria_id";
+})->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
 
-Route::get('/contact', function() {
-    return 'Contato = 4002-8922';
+Route::get('/rota1', function (){
+    echo 'Rota 1';
+})->name('site.rota1');
+
+Route::get('/rota2', function (){
+    return redirect()->route('site.rota1');
+})->name('site.rota2');
+
+Route::fallback(function () {
+   echo 'A rota acessada não existe. <a href="' .\route('site.index').'">Clique aqui </a> para retornar à página principal';
 });
